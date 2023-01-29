26495aa196067d009623aed6397d8e35a2b20e46
nojob <devin1891+nojob@gmail.com>
2023-01-29 02:14:37 -0800

    setting up controller

--- programs/rps/src/lib.rs
+++ programs/rps/src/lib.rs
@@ -1,20 +1,45 @@
 use anchor_lang::prelude::*;
-use rockpaperscissors::GameState;
+use rockpaperscissors::{
+    GameState,
+    process_action,
+    Actions,
+};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod rps {
+    use rockpaperscissors::Actions;
+
     use super::*;
 
-    pub fn initialize(ctx: Context<CreateGame>) -> Result<()> {
+    pub fn initialize(ctx: Context<InitializeGame>) -> Result<()> {
+        ctx.accounts.game.state = GameState::Initialized;
         Ok(())
     }
+
+    pub fn make_action(ctx: Context<MakeAction>, action: Actions) -> Result<()> {
+        let pubkey = ctx.accounts.game.key();
+        let clock = &Clock::get()?;
+        ctx.accounts.game.state = process_action(ctx.accounts.game.key(), ctx.accounts.game.state, action, clock.slot);
+        Ok(())
+    }
+
+}
+
+
+#[derive(Accounts)]
+pub struct InitializeGame<'info> {
+    #[account(init, payer = player, space = 10000)]
+    pub game: Account<'info, Game>,
+    #[account(mut)]
+    pub player: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-pub struct CreateGame<'info> {
-    #[account(init)]
+pub struct MakeAction<'info> {
+    #[account(mut)]
     pub game: Account<'info, Game>,
 }
 
